2a0f8c11d33b7fed9cb45929d3cee2d2c52aa3a7
Jason Gilman <jason@element84.com>
2013-08-16 12:01:52 -0400

    Added another util test.

--- src/cljs/vdd/util.cljs
+++ src/cljs/vdd/util.cljs
@@ -4,7 +4,7 @@
   (.log js/console str))
 
 (defn js-obj->map 
-  "Converts a javascript object to a map with keyword keys"
+  "Converts a javascript object to a map with keyword keys. Does not handle subobjects"
   [jo]
   (into {} (for [[k v] (js->clj jo)]
              [(keyword k) v])))
--- test/cljs/vdd/test/util.cljs
+++ test/cljs/vdd/test/util.cljs
@@ -1,14 +1,13 @@
 (ns vdd.test.util
   (:require-macros [cemerick.cljs.test :refer (is deftest with-test run-tests testing are)])
-  (:require [cemerick.cljs.test :as t])
-  (:use [vdd.util :only [js-obj->map]]))
+  (:require [cemerick.cljs.test :as t]
+            [vdd.util :as util]))
 
 (comment 
-  (js-obj->map (js/JSON.parse "{\"test\":5}"))
   (t/test-ns 'vdd.test.util)
 )
 
-
 (deftest test-js-obj->map
-  (are [json clj-obj] (= clj-obj (js-obj->map (js/JSON.parse json)))
-      "{\"a\":5}" {:a 5}))
+  (are [json clj-obj] (= clj-obj (util/js-obj->map (js/JSON.parse json)))
+      "{\"a\":5}" {:a 5}
+      "{\"a\":5,\"b\":7}" {:a 5 :b 7}))
